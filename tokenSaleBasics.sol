70d437f8297f774b027ec027aae5598eee3b2fdb
Štěpán Fau <sbren@Hexsus1.local>
2021-05-08 20:04:54 +0200

    multiple smart contract interaction

--- tokenSaleBasics.sol
+++ tokenSaleBasics.sol
@@ -1,29 +1,36 @@
 pragma solidity 0.5.1;
 
-contract myContract {
+contract ERC20Token {
     mapping(address => uint256) public balances;
+    
+    function mint() public {
+        balances[tx.origin]++;
+    }
+}
+
+contract myContract {
     address payable wallet;
+    address public token;
     
     event Purchase(
         address _buyer,
         uint256 _amount
     );
     
-    constructor(address payable _wallet) public {
+    constructor(address payable _wallet, address _token) public {
         wallet = _wallet;
-    }
-    
-    function buyTokens() public payable{
-        // buy a token
-        balances[msg.sender] += 1;
-        // send ether to the wallet
-        // there's currently no value assigned to the TX when calling this function
-        wallet.transfer(msg.value);
-        
-        emit Purchase(msg.sender, 1);
+        token = _token;
     }
     
     function() external payable {
         buyTokens();
+    }
+    
+    function buyTokens() public payable{
+        ERC20Token(address(token)).mint();
+        
+        wallet.transfer(msg.value);
+        
+        emit Purchase(msg.sender, 1);
     }
 }
